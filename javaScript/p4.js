0e2bcfa8df9625e12333be9fb612473b84a8d791
Abhimanyu Ajudiya <88022199+AbhimanyuAjudiya@users.noreply.github.com>
2022-08-23 16:30:38 +0530

    Add files via upload

---
+++ javaScript/p4.js
@@ -0,0 +1,19 @@
+function abc(){
+    var a =10;
+    var b;
+
+    b=a++;
+    console.log(a);
+    console.log(b);
+}
+function xyz(){
+    var a =10;
+    var b;
+
+    b=++a;
+    console.log(a);
+    console.log(b);
+}
+
+abc();
+xyz();
