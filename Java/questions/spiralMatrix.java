8116c9207b849a28a84ce38f86854ebf967c9809
Abhimanyu Ajudiya <88022199+AbhimanyuAjudiya@users.noreply.github.com>
2022-09-06 21:57:57 +0530

    Add files via upload

---
+++ Java/questions/spiralMatrix.java
@@ -0,0 +1,40 @@
+public class spiralMatrix {
+
+    public static void spiralMatrix(int matrix[][]) {
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length-1;
+        int endCol = matrix[0].length-1;
+        while(startRow <= endRow && startCol <= endCol){
+            //Top = Printing startRow
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(matrix[startRow][j] + " ");
+            }
+
+            //Right = Printing endCol
+            for (int i = startRow; i <= endRow; i++) {
+                System.out.print(matrix[i][endCol] + " ");
+            }
+            //Bottom = Printing endCol
+            for (int j = endCol-1; j >= startCol; j--) {
+                System.out.print(matrix[endRow][j] + " ");
+            }
+            //Left = Printing startCol
+            for (int i = endRow-1; i >= startRow+1; i--) {
+                System.out.print(matrix[i][startCol] + " ");
+            }
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{ 1, 2, 3, 4},
+                       { 5, 6, 7, 8},
+                       { 9,10,11,12},
+                       {13,14,15,16}};
+        spiralMatrix(matrix);
+    }
+}
