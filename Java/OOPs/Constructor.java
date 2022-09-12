4cbdcab40b52629d78aae5856706f8f5d23ccb19
Abhimanyu Ajudiya <88022199+AbhimanyuAjudiya@users.noreply.github.com>
2022-09-12 21:02:56 +0530

    Add files via upload

--- Java/OOPs/Constructor.java
+++ Java/OOPs/Constructor.java
@@ -4,8 +4,15 @@ public class Constructor {
         s1.name = "Abhimanyu";
         s1.rollNo = 20;
         s1.pass = "abc";
+        s1.marks[0] = 90;
+        s1.marks[1] = 80;
+        s1.marks[2] = 70;
 
         Student s2 = new Student(s1);
+        s1.marks[1] = 90;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(s2.marks[i]);
+        }
         s2.pass = "xyz";
     }
 }
@@ -15,12 +22,14 @@ class Student{
     String name;
     int rollNo;
     String pass;
+    int marks[];
 
 
     //Copy Constructor
     Student(Student s1){
         this.name = s1.name;
         this.rollNo = s1.rollNo;
+        this.marks = s1.marks;
     }
 
     Student(){
