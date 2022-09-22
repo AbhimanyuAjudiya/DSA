b86f46cdaab7f8efda0fd4bd039b2acd17121339
Abhimanyu Ajudiya <88022199+AbhimanyuAjudiya@users.noreply.github.com>
2022-09-22 19:42:53 +0530

    new program

--- Java/Recursion/TilingProblem.java
+++ Java/Recursion/TilingProblem.java
@@ -3,15 +3,17 @@ public class TilingProblem {
         if(n == 0 || n == 1){ //base case
             return 1;
         }        
-        //checking for horizontal
-        int fnm1 = tilingProblem(n-1);
-        // checking for vertical
-        int fnm2 = tilingProblem(n-2);
+        // //checking for horizontal
+        // int fnm1 = tilingProblem(n-1);
+        // // checking for vertical
+        // int fnm2 = tilingProblem(n-2);
         
-        int noOfWays = fnm1 + fnm2;
-        return noOfWays;
+        // int noOfWays = fnm1 + fnm2;
+        // return noOfWays;
+
+        return tilingProblem(n-1) + tilingProblem(n-2);
     }
     public static void main(String[] args) {
-        System.out.println(tilingProblem(5));
+        System.out.println(tilingProblem(4));
     }
 }
--- Java/Recursion/fibonacci.java
+++ Java/Recursion/fibonacci.java
@@ -3,10 +3,11 @@ public class fibonacci {
         if(n == 0 || n == 1){
             return n;
         }
-        int fib1 = fib(n-1);
-        int fib2 = fib(n-2);
-        int result = fib1 + fib2;
-        return result;
+        // int fib1 = fib(n-1);
+        // int fib2 = fib(n-2);
+        // int result = fib1 + fib2;
+        // return result;
+        return fib(n-1) + fib(n-2);
     }
     public static void main(String[] args) {
         System.out.println(fib(10));
