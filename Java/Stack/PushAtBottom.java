58c64e4f03ad25372e9e973c232335ede7e6a1f6
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-02-11 22:41:45 +0530

    stack lerning

--- Java/Stack/PushAtBottom.java
+++ Java/Stack/PushAtBottom.java
@@ -2,14 +2,14 @@ import java.util.*;;
 
 public class PushAtBottom {
 
-    public static void pushAtBottom(Stack<Integer> s, int data){
+    public static void pushAtBottom(Stack<Integer> s, int data){ 
         if(s.isEmpty()){
             s.push(data);
             return;
         }
-        int top = s.pop();
+        int top = s.pop(); // recurtion
         pushAtBottom(s, data);
-        s.push(top);
+        s.push(top);// iteration
     }
     public static void main(String[] args) {
         Stack s = new Stack<>();
