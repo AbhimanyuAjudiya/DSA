65e8ea7ccc9ba13553e5c1ec7f07074d3c1d32ac
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-03-14 23:15:40 +0530

    inOrder

--- Java/Binary Tree/BTBuild.java
+++ Java/Binary Tree/BTBuild.java
@@ -37,13 +37,24 @@ public class BTBuild {
             preOrder(root.left);
             preOrder(root.right);
         }
+
+        public static void inOrder(Node root){// O(n)
+            if(root == null){
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data+" ");
+            inOrder(root.right);
+        }
+
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree bt = new BinaryTree();
         Node root = bt.buildTree(nodes);
         // System.out.println(root.data);
-        bt.preOrder(root);
+        // bt.preOrder(root);
+        bt.inOrder(root);
         System.out.println();
     }    
 }
