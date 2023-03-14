fda4af6326642a6cbcc51a98ff0474f41808deca
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-03-15 01:44:56 +0530

    Level Order Traversal

--- Java/Binary Tree/BTBuild.java
+++ Java/Binary Tree/BTBuild.java
@@ -1,4 +1,5 @@
-import java.util.*;;
+import java.util.*;
+import java.util.LinkedList;;
 
 public class BTBuild {
 
@@ -55,6 +56,37 @@ public class BTBuild {
             postOrder(root.right);
             System.out.print(root.data+" ");
         }
+
+        //Level Order Traversal
+        public static void levelOrder(Node root){
+            if(root == null){
+                return;
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+            while(!q.isEmpty()){
+                Node currNode = q.remove();
+                if(currNode == null){
+                    System.out.println();
+                    if(q.isEmpty()){
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.out.print(currNode.data+" ");
+                    if(currNode.left != null){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right != null){
+                        q.add(currNode.right);
+                    }
+                    
+                }
+            }
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -63,7 +95,8 @@ public class BTBuild {
         // System.out.println(root.data);
         // bt.preOrder(root);
         // bt.inOrder(root);
-        bt.postOrder(root);
+        // bt.postOrder(root);
+        bt.levelOrder(root);
         System.out.println();
     }    
 }
