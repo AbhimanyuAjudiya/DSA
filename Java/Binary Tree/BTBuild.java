e5f6951e335c375434849c4e4b89694117f10ac0
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-03-14 23:01:05 +0530

    preOrder

--- Java/Binary Tree/BTBuild.java
+++ Java/Binary Tree/BTBuild.java
@@ -13,9 +13,9 @@ public class BTBuild {
         }
     }
 
-    static class BinaryTree{ //O(n)
+    static class BinaryTree{ 
         static int idx = -1;
-        public static Node buildTree(int nodes[]){
+        public static Node buildTree(int nodes[]){// O(n)
             idx++;
             if(nodes[idx] == -1){
                 return null;
@@ -27,11 +27,23 @@ public class BTBuild {
 
             return newNode;
         }
+
+        public static void preOrder(Node root){// O(n)
+            if(root == null){
+                System.out.print("-1 ");
+                return;
+            }
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree bt = new BinaryTree();
         Node root = bt.buildTree(nodes);
-        System.out.println(root.data);
+        // System.out.println(root.data);
+        bt.preOrder(root);
+        System.out.println();
     }    
 }
