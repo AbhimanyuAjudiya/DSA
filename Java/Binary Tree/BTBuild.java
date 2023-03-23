e84493c614e4834311f87c43b7a1464bff3b9568
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-03-23 20:44:51 +0530

    nothing

--- Java/Binary Tree/BTBuild.java
+++ Java/Binary Tree/BTBuild.java
@@ -126,7 +126,6 @@ public class BTBuild {
         // bt.levelOrder(root);
         // System.out.println();
 
-      
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
