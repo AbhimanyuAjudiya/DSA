19d1da3befd4a12f97c04de61012153c29f0a6b2
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-03-25 23:20:03 +0530

    h

--- Java/Binary Tree/BTBuild.java
+++ Java/Binary Tree/BTBuild.java
@@ -116,7 +116,6 @@ public class BTBuild {
         return ls + rs + root.data;
     }
     public static void main(String[] args) {
-        
         // int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         // BinaryTree bt = new BinaryTree();
         // Node root = bt.buildTree(nodes);
