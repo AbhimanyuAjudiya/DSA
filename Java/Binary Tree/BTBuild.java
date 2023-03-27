38625cd75b3b5a1a81d72ea4982479237d611a6d
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-03-27 23:24:25 +0530

    optmized Diameter in bt

--- Java/Binary Tree/BTBuild.java
+++ Java/Binary Tree/BTBuild.java
@@ -129,6 +129,26 @@ public class BTBuild {
         
         return Math.max(selfDia, Math.max(leftDia, rightDia));
     }
+
+    static class Info{
+        int dia;
+        int ht;
+
+        Info(int dia, int ht){
+            this.dia = dia;
+            this.ht = ht;
+        }
+    }
+
+    public static Info optmizedDiameter(Node root){
+        if(root == null) return new Info(0, 0);
+        Info lInfo = optmizedDiameter(root.left);
+        Info rInfo = optmizedDiameter(root.right);
+        int finalDia = Math.max(Math.max(lInfo.dia, rInfo.dia), lInfo.ht + rInfo.ht + 1);
+        int finalHt = Math.max(lInfo.ht, rInfo.ht) + 1;
+
+        return new Info(finalDia, finalHt);
+    }
     public static void main(String[] args) {
         // int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         // BinaryTree bt = new BinaryTree();
@@ -155,7 +175,7 @@ public class BTBuild {
                 4  5 6  7
          */
         // System.out.println(sumOfNodes(root));
-        System.out.println(diameter(root));
+        System.out.println(optmizedDiameter(root).dia);
 
     }    
 }
