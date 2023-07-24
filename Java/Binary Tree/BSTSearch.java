3b2b798a294670f93e3d6d3d5a22dc32f156388d
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-07-24 10:24:41 +0530

    added BST search

---
+++ Java/Binary Tree/BSTSearch.java
@@ -0,0 +1,61 @@
+public class BSTSearch {
+    static class Node{
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data){
+            this.data= data;
+        }
+    }
+
+    public static Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+
+        if(root.data > val){
+            root.left = insert(root.left, val);
+        }else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    } 
+
+    public static void inOrder(Node root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+
+    public static boolean search(Node root, int key){
+        if(root == null){
+            return false;
+        }
+
+        if(root.data == key){
+            return true;
+        }
+
+        if(root.data > key){
+            return search(root.left, key);
+        } else {
+            return search(root.right, key);
+        }
+    }
+
+    public static void main(String[] args) {
+        int val[] = {5,1,3,4,2,7};
+        Node root = null;
+        for (int i = 0; i < val.length; i++) {
+            root = insert(root, val[i]);
+        }        
+        inOrder(root);
+        System.out.println();
+        System.out.println(search(root, 6));
+    }
+}
