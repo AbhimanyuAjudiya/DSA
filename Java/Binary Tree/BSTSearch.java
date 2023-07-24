094097e9615a65956d8d2cdebb2e9045dd946e35
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-07-24 11:06:36 +0530

    in range function in BST

--- Java/Binary Tree/BSTSearch.java
+++ Java/Binary Tree/BSTSearch.java
@@ -79,6 +79,21 @@ public class BSTSearch {
         return root;
     }
 
+    public static void printInRange(Node root,int k1, int k2){
+        if(root == null){
+            return;
+        }
+        if(root.data >= k1 && root.data <= k2){
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, k1, k2);
+        } else if(root.data < k1){
+            printInRange(root.left, k1, k2);
+        } else {
+            printInRange(root.right, k1, k2);
+        }
+    }
+
     public static void main(String[] args) {
         int val[] = {8,5,3,1,4,6,10,11,14};
         Node root = null;
@@ -89,8 +104,9 @@ public class BSTSearch {
         System.out.println();
         System.out.println(search(root, 6));
 
-        delete(root, 3);
-        inOrder(root);
-        System.out.println();
+        printInRange(root, 5, 12);
+        // delete(root, 3);
+        // inOrder(root);
+        // System.out.println();
     }
 }
