e7961cab1a52e44418f9903d761b072f62a8a075
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-07-27 23:55:28 +0530

    learning priority q

--- Java/Heaps/PriorityQ.java
+++ Java/Heaps/PriorityQ.java
@@ -1,17 +1,44 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
+ class Students3 implements Comparable<Students3>{
+    String name;
+    int rank;
+
+    Students3(String name, int rank){
+        this.name = name;
+        this.rank = rank;
+    }
+
+    @Override 
+    public int compareTo(Students3 s2){
+        return this.rank - s2.rank;
+    }
+
+}
 public class PriorityQ {
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Students3> pq = new PriorityQueue<>();
+        // e -> 1
+        // c -> 4
+        // b -> 5
+        // a -> 8
+        // d -> 10
+        // PriorityQueue<Students3> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        // d -> 10
+        // a -> 8
+        // b -> 5
+        // c -> 4
+        // e -> 1
 
-        pq.add(1);
-        pq.add(2);
-        pq.add(99);
-        pq.add(9559);
-        pq.add(9);
+        pq.add(new Students3("a", 8));
+        pq.add(new Students3("b", 5));
+        pq.add(new Students3("c", 4));
+        pq.add(new Students3("d", 10));
+        pq.add(new Students3("e", 1));
 
         while(!pq.isEmpty()) {
-            System.out.println(pq.peek());
+            System.out.println(pq.peek().name+" -> "+pq.peek().rank);
             pq.remove();
         }
     }
