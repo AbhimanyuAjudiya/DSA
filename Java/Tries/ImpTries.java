e7387f4697b86d5c73d44b43d1041da83c938e5c
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-09-13 06:44:36 +0530

    Learning Tries

---
+++ Java/Tries/ImpTries.java
@@ -0,0 +1,34 @@
+public class ImpTries {
+
+    public class Node {
+        Node children[] = new Node[26];
+        boolean eow = false; //End of word
+
+        Node(){
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+        for(int level = 0; level < word.length(); level++){
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+    public static void main(String[] args) {
+        String[] word = {"the" , "a" , "there" , "their" , "any", "thee"};
+        for (int i = 0; i < word.length; i++) {
+            insert(word[i]);
+        }
+        
+    }
+}
